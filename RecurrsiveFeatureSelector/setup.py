52b8c2ebbafc35bb5941fd6141c6a17eecef3c5d
HindyDS <69097028+HindyDS@users.noreply.github.com>
2021-04-28 23:22:12 +0800

    Update setup.py

--- RecurrsiveFeatureSelector/setup.py
+++ RecurrsiveFeatureSelector/setup.py
@@ -6,17 +6,17 @@
 
 import setuptools
 
-with open('README.md', 'r') as fh:
+with open('C:/Users/Hindy/Desktop/Container/README.md', 'r') as fh:
     long_descripion = fh.read()
     
 setuptools.setup(
-    name='RecurrsiveFeatureSelector', 
+    name='Recurrsive_Feature_Selector', 
     version='0.0.3',
     author='Hindy Yuen', 
     author_email='hindy888@hotmail.com', 
     description='Recurrsively selecting the best features', 
-    long_descripion=long_descripion, 
-    long_descripion_content_type='text/markdown', 
+    long_description=long_descripion, 
+    long_description_content_type='text/markdown', 
     url='https://github.com/HindyDS/RFS', 
     packages=setuptools.find_packages(), 
     classifiers=[
