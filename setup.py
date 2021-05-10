c803f61b8b5785162fac2dab18eccf5528106bcd
Hindy <69097028+HindyDS@users.noreply.github.com>
2021-05-10 21:06:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@
 
 import setuptools
 
-with open('C:/Users/Hindy/Desktop/Container_RecurrsiveFeatureSelector/README.md', 'r') as fh:
+with open('C:/Users/Hindy/Desktop/Container_RecursiveFeatureSelector/README.md', 'r') as fh:
     long_descripion = fh.read()
     
 setuptools.setup(
-    name='RecurrsiveFeatureSelector', 
-    version='1.1.3',
+    name='RecursiveFeatureSelector', 
+    version='1.1.6',
     author='Hindy Yuen', 
     author_email='hindy888@hotmail.com',
     license='MIT',
-    description='Recurrsively selecting features for machine learning task.', 
+    description='Recursively selecting features for machine learning task.', 
     long_description=long_descripion, 
     long_description_content_type='text/markdown', 
-    url='https://github.com/HindyDS/RecurrsiveFeatureSelector', 
+    url='https://github.com/HindyDS/RecursiveFeatureSelector', 
     classifiers=[
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
@@ -28,9 +28,8 @@ setuptools.setup(
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
 ],
-    keywords='Recurrsive features selection machine learning data science computer science',
-    package_dir={"":"RecurrsiveFeatureSelector"},
-    packages=['RecurrsiveFeatureSelector'],
+    keywords='recursive recursion recursively features feature select selection machine learning data science computer science recursive feature elimination dimentionality reduction ',
+    package_dir={"":"RecursiveFeatureSelector"},
+    packages=['RecursiveFeatureSelector'],
     python_requires='>=3.6'
 )
-
